f2672582be1f4fd186710648e9d4d8354494cca8
Enochmia <126403143+Enochmia@users.noreply.github.com>
2023-10-25 01:51:17 +0900

    Update main.py
    face recognition only

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-
-
-
 huskylens.init_i2c()
 huskylens.init_mode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
 
 def on_forever():
     huskylens.request()
-    global coor
-    coor = serial.write_value("box", huskylens.readBox_ss(1, Content3.ID))
+    serial.write_value("x", huskylens.readeBox_index(1, 1, Content1.X_CENTER))
 
-while true:
-    on_forever()
-    print(coor)
+while True:
+    
+    maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CW, 0) #base speed both motors
+    
+    if huskylens.is_appear(1, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK) = True: #getting coordinates of box? maybe
+        on_forever()
+    
+        if 0 < x < 160: #following face
+            x = 160-x
+            x = x/16
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 50-x) #M1 is left side servo
+        elif 0 < x <=320:
+            x = x - 160
+            x = x/16
+            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 50-x) #M2 is left side servo
+        else:
+            pass
+
+"""
+huskylens.init_i2c()
+huskylens.init_mode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
+huskylens.request()
+serial.write_value("x", huskylens.readeBox_index(1, 1, Content1.X_CENTER))
+
+
+maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK)
+huskylens.is_appear(1, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK)
+"""
