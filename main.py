820ed6f892fd37cde7d48973459d61d62b4fefbe
Enochmia <126403143+Enochmia@users.noreply.github.com>
2023-10-24 14:58:37 +0900

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+
+huskylens.init_i2c()
+huskylens.init_mode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
+
+def on_forever():
+    huskylens.request()
+    global coor
+    coor = serial.write_value("box", huskylens.readBox_ss(1, Content3.ID))
+
+while true:
+    on_forever()
+    print(coor)
---
+++ main.ts
@@ -0,0 +1,1 @@
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
