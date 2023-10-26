419f62255bc43822d0b4d332a449f4c92c91ee46
Enochmia <126403143+Enochmia@users.noreply.github.com>
2023-10-26 10:11:45 +0900

    Update Line_tracking.py

--- Line_tracking.py
+++ Line_tracking.py
@@ -24,29 +24,30 @@ while True: #0 is when sensor light is off
 """
 
 """ #V3
+
 def mf():
   maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CW, 50)
   if maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0 or maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 0:
     maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CW, 0)
   else:
-    basic.pause(30)
+    basic.pause(5)
     maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CW, 0)
 
 
 def t_left():
-  maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 25)
+  maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 0)
   maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 50)
-  basic.pause(20)
+  basic.pause(10)
   maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CW, 0)
 
 def t_right():
-  maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 25)
-  maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 50)
-  basic.pause(20)
+  maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+  maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+  basic.pause(5)
   maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CW, 0)
 
 def check_1():
-    
+    for i in range(10
     maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CW, 50)
     
 
