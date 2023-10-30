e18fca22e1ccad1ac6dfb2802b6a79117d5902c3
Enochmia <126403143+Enochmia@users.noreply.github.com>
2023-10-30 17:26:52 +0900

    Create 1724.py

---
+++ 1724.py
@@ -0,0 +1,57 @@
+def mf():
+  maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CW, 50)
+  if maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0 or maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 0:
+    maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    return break
+  else:
+    basic.pause(3000)
+    maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    
+def mb():
+  maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+  basic.pause(3000)
+  maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CCW, 0)
+
+def t_(a):
+  if a == "l" or a == "L":
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+    basic.pause(10)
+    maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CW, 0)
+  elif a == "r" or a == "R":
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+    basic.pause(10)
+    maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CW, 0)
+  else:
+    pass
+  
+def check_1():
+  for i in range(10)
+    t_(r)
+    mf()
+    mb()
+
+def check_obstacle():
+    if maqueen.ultrasonic(PingUnit.CENTIMETERS) >= 0 and maqueen.ultrasonic(PingUnit.CENTIMETERS) <= 30:
+        maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    else:
+        maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+        basic.pause(2000)
+
+def on_forever():
+
+    maqueen.motor_run(maqueen.Motors.All, maqueen.Dir.CW, 50)
+
+    if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 0:
+        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    
+    elif maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0:
+        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+
+    else:
+        pass
+
+basic.forever(on_forever)
